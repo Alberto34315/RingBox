5406bd1f9530954b7f83996a761dcc21e4bef939
unknown <ruizserranoalberto5@gmail.com>
2020-10-29 13:13:59 +0100

    Initial commit Prueba

---
+++ app/src/main/java/com/example/ringbox/LogoActivity.java
@@ -0,0 +1,26 @@
+package com.example.ringbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+        setContentView(R.layout.activity_logo);
+    }
+}
